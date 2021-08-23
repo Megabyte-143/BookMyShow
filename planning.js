e213275fe2ae43361e97837aeb6380aefdfafc2d
Megabyte <yashygkf@gmail.com>
2021-08-23 10:53:55 +0530

    planning file

---
+++ planning.js
@@ -0,0 +1,21 @@
+//Book My Show planning
+
+//=> We need a resuable carousel
+//=> Hero Carousel
+//=> Common Carousel
+//=> Cast Carousel 
+
+//~~~~~~~~~~~~~~~~~~~Pages~~~~~~~~~~~~~~~~~~~
+
+//=> Home Page
+//=> Movie Page
+//=> Category Page
+//=> Auth Page
+//=> RazorPay Payment Gateway
+
+//~~~~~~~~~~~~~~~~~~~Plan how to build it~~~~~~~~~~~~~~~~~~~
+
+//Layouts
+//Routes
+//Pages
+//Components
