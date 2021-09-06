f9b1e4a1e6b4e646095607b3630885c1bb0dcd4d
Yash Garg <yashygkf@gmail.com>
2021-09-06 20:10:38 +0530

    common slider, poster carousel and configuration

---
+++ src/components/posterSlider/poster_slider.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Slider from "react-slick";
+
+import Poster from "../poster/postercomponent";
+
+import settings from "../../config/poster_carousel_config";
+
+const PosterSlider = (props) => {
+
+    return (
+        <>
+            <div className=" flex flex-col items-start py-4 mt-6" >
+                <h3
+                    className={
+                        `text-2xl font-bold   ${props.isDark ? "text-white" : "text-black"
+                        }`
+                    } >
+                    {props.title}
+                </h3>
+                <p className={
+                    `text-sm   ${props.isDark ? "text-white" : "text-black"
+                    }`
+                }>
+                    {props.subtitle}
+                </p>
+            </div>
+            <Slider {...settings}>
+                {props.images.map((image) => (
+                    <Poster{...image} isDark={props.isDark} />
+                ))}
+            </Slider>
+        </>
+    );
+
+}
+
+export default PosterSlider;
