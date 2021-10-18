62eba17d37537c65b33da03bdb90f4fc996af756
Yash Garg <yashygkf@gmail.com>
2021-10-18 23:13:06 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import { Route } from "react-router-dom";
+
+//axios
+import axios from "axios";
 //HOC
 import DefaultHOC from "./HOC/default.hoc";
 import MovieHOC from "./HOC/movie_hoc";
+
 //Component
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/moviePage";
+import PlaysPage from "./pages/playsPage";
 
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+//axios default settings
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.params = {};
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
   return (
     <>
       <DefaultHOC path="/" exact component={HomePage} />
       <MovieHOC path="/movie/:id" exact component={MoviePage} />
+      <DefaultHOC path="/plays" exact component={PlaysPage} />
     </>
   );
 }
